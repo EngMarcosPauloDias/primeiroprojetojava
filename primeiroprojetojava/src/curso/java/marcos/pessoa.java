79247438dbc27715786699dc68b37ac8b82043f0
EngMarcosPauloDias <109599540+EngMarcosPauloDias@users.noreply.github.com>
2022-08-01 20:52:43 -0300

    mais um commit

--- primeiroprojetojava/src/curso/java/marcos/pessoa.java
+++ primeiroprojetojava/src/curso/java/marcos/pessoa.java
@@ -1,5 +1,21 @@
 package curso.java.marcos;
 
 public class pessoa {
+	
+	private string nome;
+	private string sobrenome;
+	public string getNome() {
+		return nome;
+	}
+	public void setNome(string nome) {
+		this.nome = nome;
+	}
+	public string getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(string sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
 
 }
