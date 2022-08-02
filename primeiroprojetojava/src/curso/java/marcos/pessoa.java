606d2fc6a03ba0390c275267d6aa0250576065fe
EngMarcosPauloDias <109599540+EngMarcosPauloDias@users.noreply.github.com>
2022-08-01 21:04:14 -0300

    ok

--- primeiroprojetojava/src/curso/java/marcos/pessoa.java
+++ primeiroprojetojava/src/curso/java/marcos/pessoa.java
@@ -4,8 +4,15 @@ public class pessoa {
 	
 	private string nome;
 	private string sobrenome;
+	private int idade;
 	public string getNome() {
 		return nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
 	}
 	public void setNome(string nome) {
 		this.nome = nome;
